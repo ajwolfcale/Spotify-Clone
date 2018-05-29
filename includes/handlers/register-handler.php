ce90ef440a139677ac91948b66e0010f7187df7c
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-05-28 22:06:15 -0500

    initial master commit

---
+++ includes/handlers/register-handler.php
@@ -0,0 +1,63 @@
+<?php
+
+function sanitizeFormUsername($inputText){
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  return $inputText;
+}
+
+function sanitizeFormString($inputText){
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  $inputText = ucfirst(strtolower($inputText));
+  return $inputText;
+}
+
+function sanitizeFormPassword($inputText){
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  $inputText = ucfirst(strtolower($inputText));
+  return $inputText;
+}
+
+function validateUsername($un){
+
+}
+
+function validateFirstName($fn){
+
+}
+
+function validateLastName($ln){
+
+}
+
+function validateEmails($em, $em2){
+
+}
+
+function validatePasswords($pw, $pw2){
+
+}
+               
+
+if(isset($_POST['registerButton'])){
+  //register button was pressed
+  // echo "register button was pressed";
+  $username = sanitizeFormUsername($_POST['username']);
+  $firstName = sanitizeFormString($_POST['firstName']);
+  $lastName = sanitizeFormString($_POST['lastName']);
+  $email = sanitizeFormString($_POST['email']);
+  $email2 = sanitizeFormString($_POST['email2']);
+  $password = sanitizeFormPassword($_POST['password']);
+  $password2 = sanitizeFormPassword($_POST['password2']);
+
+  validateUsername($username);
+  validateFirstName($firstName);
+  validateLastName($lastName);
+  validateEmails($email, $email2);
+  validatePasswords($password, $password2);
+
+}
+
+?>
