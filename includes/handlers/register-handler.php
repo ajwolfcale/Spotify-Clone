9c444d54cab2c6b4edf3411a5530d963bd36b0b4
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-05-28 22:23:43 -0500

    created username validation

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -20,27 +20,7 @@ function sanitizeFormPassword($inputText){
   return $inputText;
 }
 
-function validateUsername($un){
-
-}
-
-function validateFirstName($fn){
-
-}
-
-function validateLastName($ln){
-
-}
-
-function validateEmails($em, $em2){
-
-}
-
-function validatePasswords($pw, $pw2){
-
-}
-               
-
+              
 if(isset($_POST['registerButton'])){
   //register button was pressed
   // echo "register button was pressed";
@@ -52,12 +32,8 @@ if(isset($_POST['registerButton'])){
   $password = sanitizeFormPassword($_POST['password']);
   $password2 = sanitizeFormPassword($_POST['password2']);
 
-  validateUsername($username);
-  validateFirstName($firstName);
-  validateLastName($lastName);
-  validateEmails($email, $email2);
-  validatePasswords($password, $password2);
-
+  $account->register($username, $firstName, $lastName, $email, $email2,$password, $password2);
+  
 }
 
 ?>
