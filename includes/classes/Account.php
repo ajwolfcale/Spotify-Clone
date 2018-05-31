f9435fb1ca74c521745c2f3e28800985078ecc40
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-05-31 07:08:08 -0500

    login/reg styling

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -51,7 +51,7 @@
 
 		private function insertUserDetails($un, $fn, $ln, $em, $pw) {
 			$encryptedPw = md5($pw);
-			$profilePic = "assets/images/profile-pics/head_emerald.png";
+			$profilePic = "assets/images/profile-pics/profile-pic.jpg";
 			$date = date("Y-m-d");
 
 			$result = mysqli_query($this->con, "INSERT INTO users VALUES ('', '$un', '$fn', '$ln', '$em', '$encryptedPw', '$date', '$profilePic')");
