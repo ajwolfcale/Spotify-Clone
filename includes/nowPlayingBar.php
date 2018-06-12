edefed0a2d91eaa96ae3d23956c87342495d505a
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-06-12 12:38:26 -0500

    updating name and title

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -24,6 +24,14 @@ function setTrack(trackId, newPlaylist, play) {
     $.post("includes/handlers/ajax/getSongJson.php", { songId: trackId }, function(data) {
   
       var track = JSON.parse(data);
+
+      $(".trackName span").text(track.title);
+
+      $.post("includes/handlers/ajax/getArtistJson.php", { artistId: track.artist }, function(data) {
+        var artist = JSON.parse(data);
+        $(".artistName span").text(artist.name);
+
+      });
   
       audioElement.setTrack(track.path);
       audioElement.play();
@@ -62,11 +70,11 @@ function pauseSong(){
 				<div class="trackInfo">
 
 					<span class="trackName">
-						<span>Happy Birthday</span>
+						<span></span>
 					</span>
 
 					<span class="artistName">
-						<span>Reece Kenney</span>
+						<span></span>
 					</span>
 
 				</div>
