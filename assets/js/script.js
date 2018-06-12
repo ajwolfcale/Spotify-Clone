582bf749bbf04ec19c2b3e3caeb5deeabd914065
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-06-12 13:25:37 -0500

    progress bar showing progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var currentPlaylist = [];
 var audioElement;
 
 
-function formatTime(seconds){
+let formatTime = (seconds) => {
   var time = Math.round(seconds);
   var minutes = Math.floor(time / 60);
   var seconds = time - minutes * 60;
@@ -12,6 +12,14 @@ function formatTime(seconds){
   return minutes + ":" + extraZero + seconds;
 }
 
+let updateTimeProgressBar = (audio) => {
+  $(".progressTime.current").text(formatTime(audio.currentTime));
+  $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+
+  var progress = audio.currentTime / audio.duration * 100;
+  $(".playbackBar .progress").css("width", progress + "%");
+}
+
 
 function Audio() {
 
@@ -19,9 +27,14 @@ function Audio() {
   this.audio = document.createElement('audio'); 
   
   this.audio.addEventListener("canplay", function() {
-
     var duration = formatTime(this.duration);
     $(".progressTime.remaining").text(duration);
+  })
+
+  this.audio.addEventListener("timeupdate", function() {
+    if(this.duration) {
+      updateTimeProgressBar(this);
+    }
   })
 
 	this.setTrack = function(track) {
